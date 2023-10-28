488185f72973a17f04f68243461f0a46ead19d96
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-10-29 01:21:15 +0900

    Create objektsale.js

---
+++ objektsale.js
@@ -0,0 +1,60 @@
+function input(id) {return $('#'+id).val()}
+function sale(member) {
+const res = await fetch(
+    `https://squid.subsquid.io/cosmo/graphql`,
+                        {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        query: `query {
+            transfersConnection(orderBy: timestamp_DESC, where: {from_contains: "0x000000", AND: {objekt: {collection: {member_eq: "${member}", season_eq: "Cream01", class_eq: "First"}}}}, first: 100) {
+    edges {
+      node {
+        timestamp
+        objekt {
+          serial
+          collection {
+            number
+          }
+        }
+      }
+    }
+  }
+}`
+    })
+})
+const json = await res.json()
+const data = json.data.transfersConnection
+
+/*for (i=0;i<100;i++) {
+var seeing=data.edges[i].node
+var time = new Date(parseInt(seeing.timestamp))
+var col = seeing.objekt.collection.number
+var ser = seeing.objekt.serial
+console.log([col,ser,time].join(" "))
+}*/
+  $('#that').empty();
+for (i=0;i<100;i++) {
+var seeing=data.edges[i].node
+var time = new Date(parseInt(seeing.timestamp))
+var col = seeing.objekt.collection.number
+var ser = seeing.objekt.serial
+$('#that').append("<p>"+[col,ser,time].join(" ")+"</p>")
+}
+}
+
+function heosoo(count){
+  data.edges.splice(0,count)
+}
+
+l={'101Z':'0','102Z':'0','103Z':'0','104Z':'0','105Z':'0','106Z':'0','107Z':'0','108Z':'0'}
+function saleresult(){
+for (i=0;i<data.edges.length;i++) {
+var seeing=data.edges[i].node
+var col = seeing.objekt.collection.number
+var ser = seeing.objekt.serial
+if (l[col]<ser) {l[col]=ser}
+}
+  $('#that').empty()
+$('#that').append(l)
+}
