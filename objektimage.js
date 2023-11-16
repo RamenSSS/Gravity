f64a3993ae1e697f5302223f5ae9aef7bcb4d982
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-11-16 23:41:34 +0900

    Create objektimage.js

---
+++ objektimage.js
@@ -0,0 +1,19 @@
+total = []
+
+function image(address) {
+  var next = 0
+  var res = await fetch('https://api.cosmo.fans/objekt/v1/owned-by/'+address+'?start_after='+next)
+  var data = await res.json()
+  
+  for (i=0;i<list.length;i++){
+    var seeing = list[i]
+    var testee = [...seeing.collectionId.split(' '),seeing.frontImage]
+    if (!(total.includes(testee))) {
+      total.push(testee)
+    }
+  }
+  
+  if (data.hasNext=='true') {
+    next += 30
+  } else {return}
+}
