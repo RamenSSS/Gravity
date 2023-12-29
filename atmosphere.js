145e753cd841b12d899fc8464e60d9c80972dba9
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-12-30 03:31:58 +0900

    Create atmosphere.js

---
+++ atmosphere.js
@@ -0,0 +1,19 @@
+function feed() {
+  var SA = 0
+  try {
+    SA = (parseInt(input('start_after')) || 0)
+  } catch {}
+  
+  fetch(`https://api.cosmo.fans/news/v1/feed?artist=tripleS&limit=1&start_after=${SA}`).then(async (res) => {
+    const data = await res.json()
+    const object = data.results
+
+    var feed_text = object.body
+    var image_list = object.imageUrls
+    $('#that').empty()
+    $('#that').append(`<p onclick="navigator.clipboard.writeText('${feed_text}')">${feed_text}</p>`)
+    for (i=0;i<image_list.length;i++) {
+      $('#that').append(`<img src="${image_list[i]}">`)
+    }
+  })
+}
